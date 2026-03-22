dd8c180df95aa8ad943de3688515b99e2c26926e
Gabriel Natter <nattergabriel@gmail.com>
2026-03-22 20:16:43 +0100

    feat: support installing from multiple sources at once

--- cmd/install.go
+++ cmd/install.go
@@ -17,19 +17,15 @@ var installCmd = &cobra.Command{
 	Use:     "install <user/repo[/skill][@version]>",
 	Short:   "Fetch skills from a GitHub repo into your library",
 	GroupID: groupLibrary,
-	Long: `Downloads skills from a GitHub repository and adds them to your library.
+	Long: `Downloads skills from GitHub repositories and adds them to your library.
 
 Examples:
   reseed install user/repo              # all skills from the repo
   reseed install user/repo/my-skill     # one specific skill
-  reseed install user/repo@v2.0         # pin to a tag`,
-	Args: cobra.ExactArgs(1),
+  reseed install user/repo@v2.0         # pin to a tag
+  reseed install user/repo user2/repo2  # multiple sources at once`,
+	Args: cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		ref, err := github.ParseRef(args[0])
-		if err != nil {
-			return err
-		}
-
 		lib, err := library.Open()
 		if err != nil {
 			return err
@@ -37,37 +33,39 @@ Examples:
 
 		client := github.NewClient()
 
-		// Resolve version for display
-		versionStr := ref.Version
-		if versionStr == "" {
-			versionStr = "latest"
-		}
+		for _, arg := range args {
+			ref, err := github.ParseRef(arg)
+			if err != nil {
+				return err
+			}
 
-		fmt.Printf("Fetching from %s/%s", ref.Owner, ref.Repo)
-		if ref.Skill != "" {
-			fmt.Printf("/%s", ref.Skill)
-		}
-		fmt.Printf(" (%s)...\n", versionStr)
+			versionStr := ref.Version
+			if versionStr == "" {
+				versionStr = "latest"
+			}
 
-		skills, err := client.FetchSkills(ref, lib.SkillsDir())
-		if err != nil {
-			return err
-		}
+			fmt.Printf("Fetching from %s/%s", ref.Owner, ref.Repo)
+			if ref.Skill != "" {
+				fmt.Printf("/%s", ref.Skill)
+			}
+			fmt.Printf(" (%s)...\n", versionStr)
 
-		// Track sources in config
-		for _, name := range skills {
-			lib.Config.Sources[name] = config.Source{
-				Source:  ref.SourceString(name),
-				Version: versionStr,
+			skills, err := client.FetchSkills(ref, lib.SkillsDir())
+			if err != nil {
+				return err
+			}
+
+			for _, name := range skills {
+				lib.Config.Sources[name] = config.Source{
+					Source:  ref.SourceString(name),
+					Version: versionStr,
+				}
+				fmt.Printf("  + %s\n", name)
 			}
 		}
 
 		if err := lib.SaveConfig(); err != nil {
 			return err
-		}
-
-		for _, name := range skills {
-			fmt.Printf("  + %s\n", name)
 		}
 
 		return nil
